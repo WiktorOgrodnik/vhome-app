7f3b48ed6853cb78b5640a199f741bf6c8ffb6e2
Wiktor Ogrodnik <wiktor2800-o@outlook.com>
2024-05-28 09:20:25 +0200

    Add new apiUrl
    Add android network permissions

--- lib/consts/api_url.dart
+++ lib/consts/api_url.dart
@@ -1,6 +1,9 @@
+import 'package:flutter/foundation.dart';
 import 'dart:io' show Platform;
+
+const String apiUrlRelease = "https://majanska-piramida.hopto.org";
 
 const String apiUrlLinux = "http://localhost:8080";
 const String apiUrlAndorid = "http://10.0.2.2:8080";
 
-String get apiUrl => Platform.isAndroid ? apiUrlAndorid : apiUrlLinux;  
+String get apiUrl => kReleaseMode ? apiUrlRelease : (Platform.isAndroid ? apiUrlAndorid : apiUrlLinux);  
