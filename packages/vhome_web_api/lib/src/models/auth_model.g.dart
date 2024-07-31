a4367d04bca6c18462b1c75e66c01a6c891c20b1
Wiktor Ogrodnik <wiktor2800-o@outlook.com>
2024-07-31 21:47:56 +0200

    fix wrong fuction definition

--- packages/vhome_web_api/lib/src/models/auth_model.g.dart
+++ packages/vhome_web_api/lib/src/models/auth_model.g.dart
@@ -6,12 +6,12 @@ part of 'auth_model.dart';
 // JsonSerializableGenerator
 // **************************************************************************
 
-AuthModel _$AuthModelFromJson(Map<String, dynamic> json) => AuthModel(
+AuthModel _$AuthModelFromJson(Map<String, dynamic> json, bool isGroupSelected) => AuthModel(
       id: (json['id'] as num).toInt(),
       username: json['username'] as String,
       token: json['token'] as String,
       picture: const Uint8ListConverter().fromJson(json['picture']),
-      isGroupSelected: json['isGroupSelected'] as bool? ?? false,
+      isGroupSelected: isGroupSelected,
     );
 
 Map<String, dynamic> _$AuthModelToJson(AuthModel instance) => <String, dynamic>{
--- packages/vhome_web_api/lib/src/models/user.dart
+++ packages/vhome_web_api/lib/src/models/user.dart
@@ -5,20 +5,6 @@ import 'package:vhome_web_api/src/models/models.dart';
 
 part 'user.g.dart';
 
-class Uint8ListConverter implements JsonConverter<Uint8List, dynamic> {
-  const Uint8ListConverter();
-  
-  @override
-  Uint8List fromJson(dynamic json) {
-    return json as Uint8List;
-  }
-
-  @override
-  dynamic toJson(Uint8List object) {
-    return object;
-  }
-}
-
 @JsonSerializable(explicitToJson: true)
 class User extends Equatable {
   const User({
